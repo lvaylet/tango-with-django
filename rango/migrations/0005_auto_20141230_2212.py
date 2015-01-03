4cbd586633e46991aaee76ca215588f268b36ca3
Laurent Vaylet <lvaylet@users.noreply.github.com>
2015-01-03 18:50:08 +0100

    Initial commit with legacy files

---
+++ rango/migrations/0005_auto_20141230_2212.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('rango', '0004_userprofile'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='userprofile',
+            old_name='pictures',
+            new_name='picture',
+        ),
+    ]
