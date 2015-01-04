8742ab2ead42945c789d05389eb77d01fa4b0dbd
Laurent Vaylet <lvaylet@users.noreply.github.com>
2015-01-04 18:38:12 +0100

    19.2. Add a “Like Button”

--- rango/views.py
+++ rango/views.py
@@ -304,3 +304,20 @@ def track_url(request):
                 pass
 
     return redirect(url)
+
+
+@login_required
+def like_category(request):
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['cat_id']
+
+    likes = 0
+    if cat_id:
+        cat = Category.objects.get(id=int(cat_id))
+        if cat:
+            likes = cat.likes + 1
+            cat.likes = likes
+            cat.save()
+
+    return HttpResponse(likes)
